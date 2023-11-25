ac31395e4fe3b3f9091775195930ef8f06b48586
zahidmiran <zahidmiron599@gmail.com>
2023-11-25 20:45:04 +0600

    found a way to fix the bug

--- src/component/user/Profile.js
+++ src/component/user/Profile.js
@@ -6,55 +6,70 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Profile.css";
 
 const Profile = () => {
-  const {
-    user: newUer,
-    loading,
-    isAuthenticated,
-  } = useSelector((state) => state.user) || {};
-  console.log(newUer);
+  // const {
+  //   user: newUer,
+  //   loading,
+  //   isAuthenticated,
+  // } = useSelector((state) => state.user) || {};
 
-  console.log("Test loading", loading);
+  const userData = useSelector((state) => state.user.user);
+
+  const shouldRefresh = localStorage.getItem("refreshAccountPage");
+
+  console.log(shouldRefresh);
+
   const navigate = useNavigate();
+
   // const [firstRender, setFirstRender] = useState(true);
 
   useEffect(() => {
-    if (isAuthenticated === false) {
-      navigate("/login");
+    // if (isAuthenticated === false) {
+    //   navigate("/login");
+    // }
+    if (shouldRefresh === "true") {
+      localStorage.removeItem("refreshAccountPage");
+      window.location.reload();
     }
-    if (!newUer) {
+    if (!userData) {
       <Loader />;
     }
-  }, [navigate, isAuthenticated, newUer]);
-  return (
-    <>
-      <MetaData title={`${newUer?.name}'s Profile`} />
-      <div className="profileContainer">
-        <div>
-          <h1>My Profile</h1>
-          <img src={newUer?.avatar?.url} alt={newUer?.name} />
-          <Link to="/me/update">Edit Profile</Link>
-        </div>
-        <div>
-          <div>
-            <h4>Full Name</h4>
-            <p>{newUer?.name}</p>
-          </div>
-          <div>
-            <h4>Email</h4>
-            <p>{newUer?.email}</p>
-          </div>
-          <div>
-            <h4>Joined On</h4>
-            <p>{String(newUer?.createdAt).substr(0, 10)}</p>
-          </div>
+  }, [navigate, userData, shouldRefresh]);
 
-          <div>
-            <Link to="/orders">My Orders</Link>
-            <Link to="/password/update">Change Password</Link>
-          </div>
-        </div>
-      </div>
-    </>
+  // window.onload = function () {
+  //   window.location.reload();
+  // };
+
+  return (
+    <>{userData ? <p>{userData.name} </p> : <Loader />}</>
+    // <>
+    //   <MetaData title={`${newUer?.name}'s Profile`} />
+    //   <div className="profileContainer">
+    //     <div>
+    //       <h1>My Profile</h1>
+    //       <img src={newUer?.avatar?.url} alt={newUer?.name} />
+    //       <Link to="/me/update">Edit Profile</Link>
+    //     </div>
+    //     <div>
+    //       <div>
+    //         <h4>Full Name</h4>
+    //         <p>{newUer?.name}</p>
+    //       </div>
+    //       <div>
+    //         <h4>Email</h4>
+    //         <p>{newUer?.email}</p>
+    //       </div>
+    //       <div>
+    //         <h4>Joined On</h4>
+    //         <p>{String(newUer?.createdAt).substr(0, 10)}</p>
+    //       </div>
+
+    //       <div>
+    //         <Link to="/orders">My Orders</Link>
+    //         <Link to="/password/update">Change Password</Link>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </>
   );
 };
 
