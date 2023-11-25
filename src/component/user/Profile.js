a23044888004db5a4ca425b3454e174baa3eab5e
zahidmiran <zahidmiron599@gmail.com>
2023-11-25 19:36:34 +0600

    Found New Bux

--- src/component/user/Profile.js
+++ src/component/user/Profile.js
@@ -6,10 +6,13 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Profile.css";
 
 const Profile = () => {
-  const { user, loading, isAuthenticated } =
-    useSelector((state) => state.user) || {};
+  const { user, loading, isAuthenticated } = useSelector(
+    (state) => state?.user
+  );
+  console.log("Test User", user);
+  console.log("Test loading", loading);
   const navigate = useNavigate();
-  const [firstRender, setFirstRender] = useState(true);
+  // const [firstRender, setFirstRender] = useState(true);
 
   useEffect(() => {
     if (isAuthenticated === false) {
@@ -18,29 +21,31 @@ const Profile = () => {
   }, [navigate, isAuthenticated, user]);
   return (
     <>
-      {loading && user === "undefined" ? (
+      {!user ? <Loader /> : <p> {user?.name} </p>}
+
+      {/* {loading && user === "undefined" ? (
         <Loader />
       ) : (
         <>
-          <MetaData title={`${user?.name}'s Profile`} />
+          <MetaData title={`${user.name}'s Profile`} />
           <div className="profileContainer">
             <div>
               <h1>My Profile</h1>
-              <img src={user?.avatar?.url} alt={user?.name} />
+              <img src={user.avatar.url} alt={user.name} />
               <Link to="/me/update">Edit Profile</Link>
             </div>
             <div>
               <div>
                 <h4>Full Name</h4>
-                <p>{user?.name}</p>
+                <p>{user.name}</p>
               </div>
               <div>
                 <h4>Email</h4>
-                <p>{user?.email}</p>
+                <p>{user.email}</p>
               </div>
               <div>
                 <h4>Joined On</h4>
-                <p>{String(user?.createdAt).substr(0, 10)}</p>
+                <p>{String(user.createdAt).substr(0, 10)}</p>
               </div>
 
               <div>
@@ -50,7 +55,7 @@ const Profile = () => {
             </div>
           </div>
         </>
-      )}
+      )} */}
     </>
   );
 };
